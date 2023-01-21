b13f436ef2a128962d680e7a10ddbbe898e5e518
CB-RyanMcCormick <122290337+CB-RyanMcCormick@users.noreply.github.com>
2023-01-21 16:56:23 -0600

    Change from org.json to kotlinx.serialization.json (#23)

--- DigitalTurbineExchangeAdapter/build.gradle.kts
+++ DigitalTurbineExchangeAdapter/build.gradle.kts
@@ -10,6 +10,7 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("maven-publish")
     id("com.jfrog.artifactory")
+    id("kotlinx-serialization")
 }
 
 repositories {
@@ -71,6 +72,7 @@ dependencies {
 
     // Adapter Dependencies
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
 }
 
 artifactory {
