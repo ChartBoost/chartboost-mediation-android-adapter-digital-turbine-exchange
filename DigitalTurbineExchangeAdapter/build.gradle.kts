8ea984c0fc24c67784240cc070b08dbdb4c38653
vu <vu.chau@chartboost.com>
2024-06-04 15:31:37 -0400

    API 34

--- DigitalTurbineExchangeAdapter/build.gradle.kts
+++ DigitalTurbineExchangeAdapter/build.gradle.kts
@@ -36,11 +36,11 @@ repositories {
 
 android {
     namespace = "com.chartboost.mediation.digitalturbineexchangeadapter"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         minSdk = 21
-        targetSdk = 33
+        targetSdk = 34
         // If you touch the following line, don't forget to update scripts/get_rc_version.zsh
         android.defaultConfig.versionName = System.getenv("VERSION_OVERRIDE") ?: "5.8.2.7.0"
         buildConfigField(
