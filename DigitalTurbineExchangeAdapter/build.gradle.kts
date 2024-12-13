a5c2060942a97ee7738311df3bf2d405c27a55f0
Bichen Wang <bichenwang@gmail.com>
2024-12-13 04:45:28 -0800

    compile33

--- DigitalTurbineExchangeAdapter/build.gradle.kts
+++ DigitalTurbineExchangeAdapter/build.gradle.kts
@@ -36,11 +36,11 @@ repositories {
 
 android {
     namespace = "com.chartboost.mediation.digitalturbineexchangeadapter"
-    compileSdk = 34
+    compileSdk = 33
 
     defaultConfig {
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 33
         // If you touch the following line, don't forget to update scripts/get_rc_version.zsh
         android.defaultConfig.versionName = System.getenv("VERSION_OVERRIDE") ?: "5.8.3.0.0"
         buildConfigField(
